43484b9fae1071e6e2912f3c3a3c3c1881e285ce
yuyu898 <76600806+yuyu898@users.noreply.github.com>
2022-12-06 22:12:17 -0800

    Add files via upload

---
+++ profolio/JS/gallery-change-bg-color.js
@@ -0,0 +1,169 @@
+//这一段 code 必须要放在 </body> 的后面, 如果放在 <head> 裡面就不會 work:
+
+ // image 1:
+ var img1 = document.getElementById("image1");
+ var divColor1 = document.getElementById("gallery1");
+ img1.addEventListener("mouseover", function(){
+	 img1.style.borderRadius = "10%";
+	 img1.style.boxShadow = "5px 5px 8px 0 #000000";
+	 divColor1.style.backgroundColor = "#9E9E9E";
+ })
+ img1.addEventListener("mouseout", function(){
+	  img1.style.borderRadius = "";
+	 img1.style.boxShadow = "";
+	 divColor1.style.backgroundColor = "#2f2f2f";
+ })
+
+ //image 2:
+ var img2 = document.getElementById("image2");
+ var divColor2 = document.getElementById("gallery2");
+ img2.addEventListener("mouseover", function(){
+	 img2.style.borderRadius = "10%";
+	 img2.style.boxShadow = "5px 5px 8px 0 #000000";
+	 divColor2.style.backgroundColor = "#9E9E9E";
+ })
+ img2.addEventListener("mouseout", function(){
+	 img2.style.borderRadius = "";
+	 img2.style.boxShadow = "";
+	 divColor2.style.backgroundColor = "#2f2f2f";
+ })
+
+ //image 3:
+ var img3 = document.getElementById("image3");
+ var divColor3 = document.getElementById("gallery3");
+ img3.addEventListener("mouseover", function(){
+	 img3.style.borderRadius = "10%";
+	 img3.style.boxShadow = "5px 5px 8px 0 #000000";
+	 divColor3.style.backgroundColor = "#9E9E9E";
+ })
+ img3.addEventListener("mouseout", function(){
+	 img3.style.borderRadius = "";
+	 img3.style.boxShadow = "";
+	 divColor3.style.backgroundColor = "#2f2f2f";
+ })
+
+// image 4:
+ var img4 = document.getElementById("image4");
+ var divColor4 = document.getElementById("gallery4");
+ img4.addEventListener("mouseover", function(){
+	 img4.style.borderRadius = "10%";
+	 img4.style.boxShadow = "5px 5px 8px 0 #000000";
+	 divColor4.style.backgroundColor = "#9E9E9E";
+ })
+ img4.addEventListener("mouseout", function(){
+	 img4.style.borderRadius = "";
+	 img4.style.boxShadow = "";
+	 divColor4.style.backgroundColor = "#2f2f2f";
+ })
+
+ //image 5:
+ var img5 = document.getElementById("image5");
+ var divColor5 = document.getElementById("gallery5");
+ img5.addEventListener("mouseover", function(){
+	 img5.style.borderRadius = "10%";
+	 img5.style.boxShadow = "5px 5px 8px 0 #000000";
+	 divColor5.style.backgroundColor = "#9E9E9E";
+ })
+ img5.addEventListener("mouseout", function(){
+	 img5.style.borderRadius = "";
+	 img5.style.boxShadow = "";
+	 divColor5.style.backgroundColor = "#2f2f2f";
+ })
+
+ //image 6:
+ var img6 = document.getElementById("image6");
+ var divColor6 = document.getElementById("gallery6");
+ img6.addEventListener("mouseover", function(){
+	 img6.style.borderRadius = "10%";
+	 img6.style.boxShadow = "5px 5px 8px 0 #000000";
+	 divColor6.style.backgroundColor = "#9E9E9E";
+ })
+ img6.addEventListener("mouseout", function(){
+	 img6.style.borderRadius = "";
+	 img6.style.boxShadow = "";
+	 divColor6.style.backgroundColor = "#2f2f2f";
+ })
+
+  // image 7:
+  var img7 = document.getElementById("image7");
+  var divColor7 = document.getElementById("gallery7");
+  img7.addEventListener("mouseover", function(){
+	  img7.style.borderRadius = "10%";
+	  img7.style.boxShadow = "5px 5px 8px 0 #000000";
+	  divColor7.style.backgroundColor = "#9E9E9E";
+  })
+  img7.addEventListener("mouseout", function(){
+	  img7.style.borderRadius = "";
+	  img7.style.boxShadow = "";
+	  divColor7.style.backgroundColor = "#2f2f2f";
+  })
+ 
+  //image 8:
+  var img8 = document.getElementById("image8");
+  var divColor8 = document.getElementById("gallery8");
+  img8.addEventListener("mouseover", function(){
+	  img8.style.borderRadius = "10%";
+	  img8.style.boxShadow = "5px 5px 8px 0 #000000";
+	  divColor8.style.backgroundColor = "#9E9E9E";
+  })
+  img8.addEventListener("mouseout", function(){
+	  img8.style.borderRadius = "";
+	  img8.style.boxShadow = "";
+	  divColor8.style.backgroundColor = "#2f2f2f";
+  })
+ 
+  //image 9:
+  var img9 = document.getElementById("image9");
+  var divColor9 = document.getElementById("gallery9");
+  img9.addEventListener("mouseover", function(){
+	  img9.style.borderRadius = "10%";
+	  img9.style.boxShadow = "5px 5px 8px 0 #000000";
+	  divColor9.style.backgroundColor = "#9E9E9E";
+  })
+  img9.addEventListener("mouseout", function(){
+	  img9.style.borderRadius = "";
+	  img9.style.boxShadow = "";
+	  divColor9.style.backgroundColor = "#2f2f2f";
+  })
+ 
+ // image 10:
+  var img10 = document.getElementById("image10");
+  var divColor10 = document.getElementById("gallery10");
+  img10.addEventListener("mouseover", function(){
+	  img10.style.borderRadius = "10%";
+	  img10.style.boxShadow = "5px 5px 8px 0 #000000";
+	  divColor10.style.backgroundColor = "#9E9E9E";
+  })
+  img10.addEventListener("mouseout", function(){
+	  img10.style.borderRadius = "";
+	  img10.style.boxShadow = "";
+	  divColor10.style.backgroundColor = "#2f2f2f";
+  })
+ 
+  //image 11:
+  var img11 = document.getElementById("image11");
+  var divColor11 = document.getElementById("gallery11");
+  img11.addEventListener("mouseover", function(){
+	  img11.style.borderRadius = "10%";
+	  img11.style.boxShadow = "5px 5px 8px 0 #000000";
+	  divColor11.style.backgroundColor = "#9E9E9E";
+  })
+  img11.addEventListener("mouseout", function(){
+	  img11.style.borderRadius = "";
+	  img11.style.boxShadow = "";
+	  divColor11.style.backgroundColor = "#2f2f2f";
+  })
+ 
+  //image 12:
+  var img12 = document.getElementById("image12");
+  var divColor12 = document.getElementById("gallery12");
+  img12.addEventListener("mouseover", function(){
+	  img12.style.borderRadius = "10%";
+	  img12.style.boxShadow = "5px 5px 8px 0 #000000";
+	  divColor12.style.backgroundColor = "#9E9E9E";
+  })
+  img12.addEventListener("mouseout", function(){
+	  img12.style.borderRadius = "";
+	  img12.style.boxShadow = "";
+	  divColor12.style.backgroundColor = "#2f2f2f";
+  })
---
+++ profolio/JS/gallery-change-pg.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+$(document).ready(function(){
+	$('#gallery').load("gallery1.html");
+	btn1.style.border= "1px solid red";
+});
+
+var btn1 = document.getElementById("buttonNumber1");
+var btn2 = document.getElementById("buttonNumber2");
+var btn3 = document.getElementById("buttonNumber3");
+var btn4 = document.getElementById("buttonNumber4");
+var btn5 = document.getElementById("buttonNumber5");
+			
+btn1.addEventListener("click", function(){
+	$('#gallery').load("gallery1.html");
+	btn1.style.border= "1px solid red";
+	btn2.style.border= "";
+	btn3.style.border= "";
+	btn4.style.border= "";
+	btn5.style.border= "";
+})
+			
+btn2.addEventListener("click", function(){
+	$('#gallery').load("gallery2.html");
+	btn1.style.border= "";
+	btn2.style.border= "1px solid red";
+	btn3.style.border= "";
+	btn4.style.border= "";
+	btn5.style.border= "";
+})
+btn3.addEventListener("click", function(){
+	$('#gallery').load("gallery3.html");
+	btn1.style.border= "";
+	btn2.style.border= "";
+	btn3.style.border= "1px solid red";
+	btn4.style.border= "";
+	btn5.style.border= "";
+})
+btn4.addEventListener("click", function(){
+	$('#gallery').load("gallery4.html");
+	btn1.style.border= "";
+	btn2.style.border= "";
+	btn3.style.border= "";
+	btn4.style.border= "1px solid red";
+	btn5.style.border= "";
+})
+btn5.addEventListener("click", function(){
+	$('#gallery').load("gallery5.html");
+	btn1.style.border= "";
+	btn2.style.border= "";
+	btn3.style.border= "";
+	btn4.style.border= "";
+	btn5.style.border= "1px solid red";
+})
