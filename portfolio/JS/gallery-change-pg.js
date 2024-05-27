dc1568e8ec79b3cb2219a2533c581547c2a8f8a8
Cliff Wu <cccw666@msn.com>
2024-05-27 15:59:26 -0700

    update portfolio section

--- portfolio/JS/gallery-change-pg.js
+++ portfolio/JS/gallery-change-pg.js
@@ -9,6 +9,7 @@ var btn2 = document.getElementById("buttonNumber2");
 var btn3 = document.getElementById("buttonNumber3");
 var btn4 = document.getElementById("buttonNumber4");
 var btn5 = document.getElementById("buttonNumber5");
+var btn6 = document.getElementById("buttonNumber6");
 			
 btn1.addEventListener("click", function(){
 	$('#gallery').load("gallery1.html");
@@ -17,6 +18,7 @@ btn1.addEventListener("click", function(){
 	btn3.style.border= "";
 	btn4.style.border= "";
 	btn5.style.border= "";
+	btn6.style.border= "";
 })
 			
 btn2.addEventListener("click", function(){
@@ -26,7 +28,9 @@ btn2.addEventListener("click", function(){
 	btn3.style.border= "";
 	btn4.style.border= "";
 	btn5.style.border= "";
+	btn6.style.border= "";
 })
+
 btn3.addEventListener("click", function(){
 	$('#gallery').load("gallery3.html");
 	btn1.style.border= "";
@@ -34,7 +38,9 @@ btn3.addEventListener("click", function(){
 	btn3.style.border= "1px solid red";
 	btn4.style.border= "";
 	btn5.style.border= "";
+	btn6.style.border= "";
 })
+
 btn4.addEventListener("click", function(){
 	$('#gallery').load("gallery4.html");
 	btn1.style.border= "";
@@ -42,7 +48,9 @@ btn4.addEventListener("click", function(){
 	btn3.style.border= "";
 	btn4.style.border= "1px solid red";
 	btn5.style.border= "";
+	btn6.style.border= "";
 })
+
 btn5.addEventListener("click", function(){
 	$('#gallery').load("gallery5.html");
 	btn1.style.border= "";
@@ -50,4 +58,15 @@ btn5.addEventListener("click", function(){
 	btn3.style.border= "";
 	btn4.style.border= "";
 	btn5.style.border= "1px solid red";
+	btn6.style.border= "";
+})
+
+btn6.addEventListener("click", function(){
+	$('#gallery').load("gallery6.html");
+	btn1.style.border= "";
+	btn2.style.border= "";
+	btn3.style.border= "";
+	btn4.style.border= "";
+	btn5.style.border= "";
+	btn6.style.border= "1px solid red";
 })
